b51da025d1e485af56662bd1fe2baf92bc858142
Ricardo <106403365+roicoroyAmigao@users.noreply.github.com>
2022-08-25 10:46:45 +0100

    Add products

--- server/data/index.js
+++ server/data/index.js
@@ -1,8 +1,10 @@
 const getUsers = require('./json/getUsers.json');
 const getConference = require('./json/getConference.json');
+const getProducts = require('./json/getProducts.json');
 
 module.exports = {
   getUsers: getUsers,
-  getConference: getConference
+  getConference: getConference,
+  getProducts: getProducts
 };
 
--- server/server.js
+++ server/server.js
@@ -5,15 +5,18 @@ const server = jsonServer.create();
 server.use(middleware);
 server.use(jsonServer.bodyParser);
 
-const userData = require('../server/data/users');
-const conferenceData = require('../server/data/users');
+const appData = require('./data');
 
 server.get('/api/users', (req, res, next) => {
-  res.status(200).send(userData.getUsers);
+  res.status(200).send(appData.getUsers);
 });
 
 server.get('/api/conference', (req, res, next) => {
-  res.status(200).send(userData.getConference);
+  res.status(200).send(appData.getConference);
+});
+
+server.get('/api/products', (req, res, next) => {
+  res.status(200).send(appData.getProducts);
 });
 
 server.listen(3000, () => {
